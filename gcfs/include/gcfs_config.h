94c51cd130d3bf4b96e5bf4d9c265f374c8ebf29
Josef Kejzlar <cuchac@email.cz>
2010-09-28 19:32:32 +0200

    GCFS: Include INI-file parsing library and basic confguration loading support

--- gcfs/include/gcfs_config.h
+++ gcfs/include/gcfs_config.h
@@ -1,24 +1,42 @@
 #ifndef GCFS_CONFIG_H
 #define GCFS_CONFIG_H
 
+class GCFS_Service;
+
 #include <string>
 #include <vector>
+#include <map>
 
 class GCFS_Config
 {
-	// Service
-	class Service
-	{
-	public:
-										Service(const char* sName):m_sName(sName){};
-								
-	public:
-		std::string					m_sName;
-	};
+
+	// Configuration Loading
+public:
+	bool								loadConfig();
 
 public:
-	std::vector<Service> 		m_vServices;
-	std::vector<std::string>	m_vServiceNames;
+	std::string						m_sDataDir;
+
+private:
+	bool								getHomePath(std::string &buffer);
+
+
+	// Services management
+public:
+	bool					AddService(const char* sDriver, const char* sName);
+
+	size_t				GetServiceCount();
+
+	GCFS_Service*		GetService(int iIndex);
+	GCFS_Service*		GetService(const char * sName);
+	int					GetServiceIndex(const char * sName);
+	
+private:
+	std::vector<GCFS_Service*> 		m_vServices;
+	std::map<std::string, int>			m_mNameToService;
+public:
+	// Vector of all available services
+	std::vector<std::string>			m_vServiceNames;
 
 };
 
--- gcfs/src/gcfs_config.cpp
+++ gcfs/src/gcfs_config.cpp
@@ -1,2 +1,99 @@
-#include "../include/gcfs_config.h"
+#include "gcfs_config.h"
+#include "gcfs_service.h"
 
+#include "lib/simpleini/SimpleIni.h"
+
+#ifdef _WIN32
+	//#include <windows.h>
+#else
+	#include <unistd.h>
+	#include <sys/types.h>
+	#include <pwd.h>
+#endif
+
+
+bool GCFS_Config::loadConfig()
+{
+	CSimpleIniA ini;
+	ini.SetUnicode();
+
+	std::string sHomePath;
+	getHomePath(sHomePath);
+	
+	ini.LoadFile((sHomePath+"/.config/gcfs/config.conf").c_str());
+
+	m_sDataDir = ini.GetValue("Global", "dataPath", (sHomePath+"/.local/gcfs/data/").c_str());
+
+	CSimpleIniA::TNamesDepend vSections;
+	ini.GetAllSections(vSections);
+
+	CSimpleIniA::TNamesDepend::iterator it;
+	for(it = vSections.begin(); it != vSections.end(); ++it)
+	{
+		if(it->pItem == "Global")
+			continue;
+
+		
+		
+	}
+}
+
+bool GCFS_Config::getHomePath(std::string &buffer)
+{
+	// Get Home dir by platfomr-independent way
+#ifdef _WIN32
+
+	buffer = getenv("USERPROFILE");
+	if(!buffer.length())
+	{
+		buffer = getenv("HOMEDRIVE");
+		buffer += getenv("HOMEPATH");
+	}
+	
+#else
+
+	buffer = getenv("HOME");
+
+	if(!buffer.length())
+		buffer = getpwuid(getuid())->pw_dir;
+	
+#endif
+}
+
+// Service management
+
+bool GCFS_Config::AddService(const char * sDriver, const char * sName)
+{
+	GCFS_Service * tmpService = GCFS_Service::createService(sDriver, sName);
+	m_vServices.push_back(tmpService);
+	m_vServiceNames.push_back(sName);
+	m_mNameToService[sName] = m_vServices.size()-1;
+}
+
+size_t GCFS_Config::GetServiceCount()
+{
+	return (int)m_vServices.size();
+}
+
+GCFS_Service* GCFS_Config::GetService(int iIndex)
+{
+	return m_vServices[iIndex];
+}
+
+GCFS_Service* GCFS_Config::GetService(const char * sName)
+{
+	std::map<std::string, int>::iterator it;
+	if((it = m_mNameToService.find(sName)) != m_mNameToService.end())
+		return m_vServices[it->second];
+	else
+		return NULL;
+}
+
+int GCFS_Config::GetServiceIndex(const char * sName)
+{
+	std::map<std::string, int>::iterator it;
+	if((it = m_mNameToService.find(sName)) != m_mNameToService.end())
+		return it->second;
+	else
+		return -1;
+}
