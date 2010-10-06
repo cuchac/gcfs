21df17166cea3a87f62a79ab112628aeef1680da
Josef Kejzlar <cuchac@email.cz>
2010-10-07 01:36:22 +0200

    GCFS: Move default configuration into separate file

---
+++ gcfs/include/gcfs_config_default.h
@@ -0,0 +1,15 @@
+#define GCFS_CONFIG_DEFAULTCONFIG		\
+"# This is configuration file for Gcfs - Grid Control File System\n\
+# Comments start with symbol '#'\n\
+# Uncoment and modify values\n\
+\n\
+#### Global settings #####\n\
+[Global]\n\
+## Path where temporary files will be stored\n\
+#dataPath = /home/joe/.local/gcfs/\n\
+\n\
+#### Services definitions ####\n\
+## Every category represents one service named according to category name.\n\
+#[Condor]\n\
+## Driver defines the grid framework to use\n\
+#driver = condor\n "
