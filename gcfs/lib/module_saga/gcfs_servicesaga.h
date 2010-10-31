ff211b9b3f04f7e83b93a442aa5994b109bd421f
Josef Kejzlar <cuchac@email.cz>
2010-10-31 05:59:53 +0100

    GCFS: First version of vorking SAGA module!!! :)

--- gcfs/lib/module_saga/gcfs_servicesaga.h
+++ gcfs/lib/module_saga/gcfs_servicesaga.h
@@ -3,16 +3,31 @@
 
 #include "gcfs_service.h"
 
+namespace saga {
+namespace job {
+class service;
+class istream;
+}
+}
+
 
 class GCFS_ServiceSaga : public GCFS_Service
 {
 public:
 									GCFS_ServiceSaga(const char * sName);
+								  ~GCFS_ServiceSaga();
 									
 public:
 	virtual bool 				submitTask(GCFS_Task* pTask);
 	virtual bool 				waitForTask(GCFS_Task* pTask);
-    
+
+public:
+	GCFS_Task::Status			getTaskStatus(GCFS_Task* pTask);
+	GCFS_Task::Status 		storeStream(saga::job::istream* pStream, GCFS_Task::File* pFile);
+	
+private:
+	saga::job::service* 		m_pJobService;
+	
 };
 
 #endif // GCFS_SERVICESAGA_H
